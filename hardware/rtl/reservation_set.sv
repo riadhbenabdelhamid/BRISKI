adebfacb5dd9a3b4a41b259695734156ae7a1e11
riadhbenabdlhamid <riadh.benabdlhamid@gmail.com>
2024-08-04 03:58:03 +0200

    Added conditions to remove live lock from LR/SC pairing and guarantee success of atomic operation

--- hardware/rtl/reservation_set.sv
+++ hardware/rtl/reservation_set.sv
@@ -1,27 +1,37 @@
-module reservation_set (
+module reservation_set #(
+    parameter integer NUM_THREADS = 16
+) (
     input logic clk,
     input logic reset,
     input logic [11:0] i_addr,
     input logic i_store_op,
     input logic i_store_cond_op,
     input logic i_load_reserved_op,
+    input logic [$clog2(NUM_THREADS)-1:0] i_mhartid,
     output logic o_sc_success
 );
 
   logic [11:0] reserved_address ;
+  logic [$clog2(NUM_THREADS)-1:0] reserving_hart ;
   logic reserved_valid;
   logic address_match ;
+  logic hart_match ;
   assign address_match = (i_addr == reserved_address) ? 1'b1 : 1'b0;
+  assign hart_match = (i_mhartid == reserving_hart) ? 1'b1 : 1'b0;
 
   always_ff @(posedge clk) begin
     if (reset) begin
       reserved_valid <= 1'b0;
     end else begin
-      if (i_load_reserved_op) begin
+      if (i_load_reserved_op & !reserved_valid) 
+      //if (i_load_reserved_op) 
         reserved_valid <= 1'b1;
-      end else if (i_store_cond_op || (i_store_op && address_match)) begin
+
+      if (i_store_cond_op && hart_match && address_match) 
         reserved_valid <= 1'b0;
-      end
+
+      if (i_store_op && address_match) 
+	      reserved_valid <= 1'b0;
     end
   end
 
@@ -29,13 +39,15 @@ module reservation_set (
     if (reset) begin
       o_sc_success <= 1'b0;
     end else begin
-      o_sc_success <= reserved_valid && address_match && i_store_cond_op;
+      o_sc_success <= reserved_valid && address_match && i_store_cond_op && hart_match;
     end
   end
 
   always_ff @(posedge clk) begin
-    if (i_load_reserved_op) begin
+    if (i_load_reserved_op & !reserved_valid) begin
+    //if (i_load_reserved_op) begin
       reserved_address <= i_addr;
+      reserving_hart <= i_mhartid;
     end
   end
 
