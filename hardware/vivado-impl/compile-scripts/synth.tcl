16a94d0ea277c9b01030b27cb9be1a427207c2e8
riadhbenabdlhamid <riadh.benabdlhamid@gmail.com>
2024-08-27 11:54:30 +0200

    Added commented out options for flatten_hierarchy

--- hardware/vivado-impl/compile-scripts/synth.tcl
+++ hardware/vivado-impl/compile-scripts/synth.tcl
@@ -1,6 +1,8 @@
 set time_1 [clock seconds]
 #synth_design -top ${TOP_RTL} -part $FPGA_PART -directive PerformanceOptimized -retiming -shreg_min_size 10 -flatten_hierarchy rebuilt 
 synth_design -top ${TOP_RTL} -part $FPGA_PART -directive AreaOptimized_High -retiming -shreg_min_size 10 -flatten_hierarchy rebuilt 
+#synth_design -top ${TOP_RTL} -part $FPGA_PART -directive AreaOptimized_High -retiming -shreg_min_size 10 -flatten_hierarchy full
+#synth_design -top ${TOP_RTL} -part $FPGA_PART -directive AreaOptimized_High -retiming -shreg_min_size 10 -flatten_hierarchy none
 set time_2 [clock seconds]
 puts "Elapsed time (Synth step)= [expr [expr $time_2 - $time_1] / 3600] : [expr [expr [expr $time_2 - $time_1] / 60] % 3600] : [expr [expr $time_2 - $time_1] % 60]"
 write_checkpoint -force $outputDir/post_synth
